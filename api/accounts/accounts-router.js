541a9daf2f2e4cf4932b34d62c7d09ab17dbb76a
Austin Johnson <austintjohnson92@gmail.com>
2022-06-13 16:20:33 -0700

    final solution

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -3,49 +3,51 @@ const md = require('./accounts-middleware')
 const Account = require('./accounts-model')
 
 router.get('/', async (req, res, next) => {
-  
-  try {
-    const accounts = await Account.getAll()
-    res.json(accounts)
-  } catch (err) {
-    next(err)
-  }
+
+    try {
+        const accounts = await Account.getAll()
+        res.json(accounts)
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.get('/:id', md.checkAccountId, (req, res, next) => {
-   res.json(req.account)
+    res.json(req.account)
 })
 
 router.post('/', md.checkAccountPayload, md.checkAccountNameUnique, async (req, res, next) => {
-  try {
-    const data = await Account.create(req.body)
-    res.status(201).json(data)
-  } catch (err) {
-    next(err)
-  }
+    try {
+        const data = await Account.create({ name: req.body.name.trim(), budget: req.body.budget })
+        res.status(201).json(data)
+    } catch (err) {
+        next(err)
+    }
 })
 
-router.put('/:id', md.checkAccountId, md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
-  try {
-    res.json('update account')
-  } catch (err) {
-    next(err)
-  }
+router.put('/:id', md.checkAccountId, md.checkAccountPayload, md.checkAccountNameUnique, async (req, res, next) => {
+    
+    try {
+        const updated = await Account.updateById(req.params.id, req.body)
+        res.json(updated)
+    } catch (err) {
+        next(err)
+    }
 });
 
 router.delete('/:id', md.checkAccountId, async (req, res, next) => {
- try {
-    await Account.deleteById(req.params.id)
-    res.json(req.account)
-  } catch (err) {
-    next(err)
-  }
+    try {
+        await Account.deleteById(req.params.id)
+        res.json(req.account)
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
- res.status(err.status || 500).json({
-    message: err.message,
- })
+    res.status(err.status || 500).json({
+        message: err.message,
+    })
 })
 
 module.exports = router;
